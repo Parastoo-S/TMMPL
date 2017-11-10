5a6114398bc5f75b098c70df461985a83cbe09fe
Parasto Saharkhiz <psaha091@uottawa.ca>
2017-11-09 23:57:24 -0500

    Added user list class

---
+++ app/src/main/java/segproject/tmmpl/UserList.java
@@ -0,0 +1,39 @@
+package segproject.tmmpl;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class UserList extends ArrayAdapter<User> {
+
+    private Activity context;
+    List<User> users;
+
+    public UserList(Activity context, List<User> users) {
+        super(context, R.layout.layout_user_list, users);
+        this.context = context;
+        this.users = users;
+    }
+
+
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View listViewItem = inflater.inflate(R.layout.layout_user_list, null, true);
+
+        TextView textViewName = (TextView) listViewItem.findViewById(R.id.textViewUsername);
+//        TextView textViewPrice = (TextView) listViewItem.findViewById(R.id.textViewPrice);
+
+        User product = users.get(position);
+        textViewName.setText(product.getUsername());
+//        textViewPrice.setText(String.valueOf(product.getPrice()));
+        return listViewItem;
+    }
+}
