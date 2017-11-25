b67faa1541cbc6d15d31d7613c05f04ce7f3dfd5
Lori Antranikian <loriantranikian@yahoo.ca>
2017-11-25 14:03:03 -0500

    shopping java doc

--- app/src/main/java/segproject/tmmpl/Shopping.java
+++ app/src/main/java/segproject/tmmpl/Shopping.java
@@ -1,13 +1,21 @@
 package segproject.tmmpl;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Shopping extends AppCompatActivity {
+    private ArrayList<String> shopping_List;
+    private ArrayAdapter<String> adapter;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +24,31 @@ public class Shopping extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        ListView listView = (ListView)findViewById(R.id.shoppingList) ;
+        String[] newItems={};
+        shopping_List=new ArrayList<>(Arrays.asList(newItems));
+        adapter = new ArrayAdapter<String>(this,R.layout.list_item,R.id.shoppingList,shopping_List);
+        listView.setAdapter(adapter);
+        editText=(EditText) findViewById(R.id.newitembtn);
+        Button newitembtn=(Button) findViewById(R.id.newitembtn);
+        newitembtn.setOnClickListener(new View.OnClickListener(){
+
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                String newItem =editText.getText().toString();
+                shopping_List.add(newItem);
+                adapter.notifyDataSetChanged();
             }
         });
+
+        }
     }
 
-}
+
+
+
+
+
+
