f8825446cb2e28fd4e48224b359767007f8fa9db
Parasto Saharkhiz <psaha091@uottawa.ca>
2017-11-25 16:06:01 -0500

    task class

---
+++ app/src/main/java/segproject/tmmpl/Task.java
@@ -0,0 +1,12 @@
+package segproject.tmmpl;
+
+/**
+ * Created by Parastoo on 11/25/2017.
+ */
+
+public class Task {
+    private String _id;
+    private String _taskname;
+//    private User user;
+    
+}
