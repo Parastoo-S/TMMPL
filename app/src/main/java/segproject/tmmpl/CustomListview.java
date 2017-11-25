0863b8afe98c254a9164f8f2fde2262e94e0e247
MahdiEtemad <Mahdietemad12@hotmail.com>
2017-11-25 13:06:46 -0500

    updated to work w/ other users

---
+++ app/src/main/java/segproject/tmmpl/CustomListview.java
@@ -0,0 +1,100 @@
+package segproject.tmmpl;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import android.app.Activity;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+/**
+ * Created by Mahdi on 11/23/2017.
+ */
+
+public class CustomListview extends ArrayAdapter<String> {
+
+    private String[] fruitname;
+    private String[] desc;
+    private Integer[] imgid;
+    private CheckBox checkBox;
+    private Activity context;
+
+    public CustomListview(Activity context, String[] fruitname, String[] desc, Integer[] imgid , CheckBox checkBox){
+        super(context,R.layout.listview_layout,fruitname);
+
+        this.context=context;
+        this.fruitname=fruitname;
+        this.desc=desc;
+        this.imgid=imgid;
+        this.checkBox=checkBox;
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+
+        View r=convertView;
+        ViewHolder viewHolder =null;
+
+        if(r==null){
+
+            LayoutInflater layoutInflater = context.getLayoutInflater();
+            r=layoutInflater.inflate(R.layout.listview_layout,null,true);
+            viewHolder=new ViewHolder(r);
+            r.setTag(viewHolder);
+
+
+        }else{
+            viewHolder=(ViewHolder)r.getTag();
+        }
+
+
+        viewHolder.ivw.setImageResource(imgid[position]);
+
+        viewHolder.tvw1.setText(fruitname[position]);
+
+        viewHolder.tvw2.setText(desc[position]);
+
+        viewHolder.chb.setChecked(false);
+
+
+
+        return r;
+    }
+
+
+    class ViewHolder{
+
+        TextView tvw1;
+        TextView tvw2;
+        ImageView ivw;
+        CheckBox chb;
+
+        ViewHolder(View v){
+            tvw1 = (TextView)v.findViewById(R.id.tvfruitname);
+            tvw2 = (TextView)v.findViewById(R.id.tvdescription);
+            ivw=(ImageView)v.findViewById(R.id.imageView);
+            chb=(CheckBox)v.findViewById(R.id.checkBox);
+        }
+
+    }
+}
---
+++ app/src/main/java/segproject/tmmpl/MainActivity.java
@@ -0,0 +1,28 @@
+package segproject.tmmpl;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
+import android.widget.ListView;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lst;
+    String[] fruitname = {"Clean Car","Take Out Trash","Clean Room","Pickup Allie","Cancel Dentist Appointment","Go Shopping"};
+    String[] desc={"Deadline: 11/15/2017","Deadline: 11/12/2017","Note: Vaccume afterwards too","Deadline: 11/11/2017 at 3:15pm", "Deadline: 11/10/2017","Note: Do it ASAP"};
+    Integer[] imgid={R.drawable.i1,R.drawable.i2,R.drawable.i6,R.drawable.plus,R.drawable.i4,R.drawable.i3};
+    CheckBox checkBox;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lst=(ListView)findViewById(R.id.listview);
+        CustomListview customListview = new CustomListview(this,fruitname,desc,imgid,checkBox);
+        lst.setAdapter(customListview);
+    }
+}
