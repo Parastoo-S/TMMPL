d7a1c99731fc784ebd5c1323d1a97c81f1f1bff1
Parasto Saharkhiz <psaha091@uottawa.ca>
2017-12-04 00:19:28 -0500

    Added checkbox

--- app/src/main/java/segproject/tmmpl/TaskList.java
+++ app/src/main/java/segproject/tmmpl/TaskList.java
@@ -55,19 +55,20 @@ public class TaskList extends ArrayAdapter<Task> {
         TextView textViewDescription = (TextView) listViewItem.findViewById(R.id.textViewDescription);
         TextView dueDateTime = (TextView) listViewItem.findViewById(R.id.dueDateTime);
 
-        //final TextView status = (TextView) listViewItem.findViewById(R.id.status);
+        final TextView status = (TextView) listViewItem.findViewById(R.id.status);
 
-      //  final CheckBox completed = (CheckBox)listViewItem.findViewById(R.id.completed);
+        final CheckBox completed = (CheckBox)listViewItem.findViewById(R.id.completed);
+        completed.setFocusable(false);
         ImageView profilePicImage = (ImageView)listViewItem.findViewById(R.id.profilePicImage);
-        Button statusBtn = (Button)listViewItem.findViewById(R.id.statusBtn);
-      /*  completed.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+//        Button statusBtn = (Button)listViewItem.findViewById(R.id.statusBtn);
+        completed.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
 
 
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(completed.isChecked()==false){
 
-                    status.setText("incomplete");
+                    status.setText("Incomplete");
 
                 }
 
@@ -77,36 +78,36 @@ public class TaskList extends ArrayAdapter<Task> {
 
                     status.setText("Complete");
                     Toast.makeText(getContext(), "Task Completed",
-                            Toast.LENGTH_LONG).show();
+                            Toast.LENGTH_SHORT).show();
 
                 }
-            }
-        });*/
-
-        statusBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                final Dialog dialog = new Dialog(context);
-                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-                dialog.setContentView(R.layout.radiobutton_dialog);
-                List<String> stringList = new ArrayList<>();  // here is list
-                                   /*  for (int i = 0; i < 5; i++) {
-                                         stringList.add("RadioButton " + (i + 1));
-                                     }*/
-                stringList.add("Complete");
-                stringList.add("Inomplete");
-                stringList.add("Defferred");
-
-                RadioGroup rg = (RadioGroup) dialog.findViewById(R.id.radio_group);
-
-                for (int i = 0; i < stringList.size(); i++) {
-                    RadioButton rb = new RadioButton(context); // dynamically creating RadioButton and adding to RadioGroup.
-                    rb.setText(stringList.get(i));
-                    rg.addView(rb);
-                }
-
-                dialog.show();
             }
         });
+
+//        statusBtn.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View view) {
+//                final Dialog dialog = new Dialog(context);
+//                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+//                dialog.setContentView(R.layout.radiobutton_dialog);
+//                List<String> stringList = new ArrayList<>();  // here is list
+//                                   /*  for (int i = 0; i < 5; i++) {
+//                                         stringList.add("RadioButton " + (i + 1));
+//                                     }*/
+//                stringList.add("Complete");
+//                stringList.add("Inomplete");
+//                stringList.add("Defferred");
+//
+//                RadioGroup rg = (RadioGroup) dialog.findViewById(R.id.radio_group);
+//
+//                for (int i = 0; i < stringList.size(); i++) {
+//                    RadioButton rb = new RadioButton(context); // dynamically creating RadioButton and adding to RadioGroup.
+//                    rb.setText(stringList.get(i));
+//                    rg.addView(rb);
+//                }
+//
+//                dialog.show();
+//            }
+//        });
 //        TextView textViewCreatorName = (TextView) listViewItem.findViewById(R.id.textViewCreatorName);
 //        TextView textViewAssignedUserName = (TextView) listViewItem.findViewById(R.id.textViewAssignedUserName);
 
