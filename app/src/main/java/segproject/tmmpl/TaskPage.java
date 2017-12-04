abe79da53010a8af1a1a974fe9298890b6e7b56a
Lori Antranikian <loriantranikian@yahoo.ca>
2017-12-04 01:28:55 -0500

    taskpage

--- app/src/main/java/segproject/tmmpl/TaskPage.java
+++ app/src/main/java/segproject/tmmpl/TaskPage.java
@@ -5,29 +5,15 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ListView;
-
-
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.CheckBox;
-import android.widget.ListView;
 import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.ToggleButton;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -35,7 +21,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -232,6 +217,32 @@ public class TaskPage extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    public void goToTasks(MenuItem menuItem){
+        Intent newActivity = new Intent(TaskPage.this, TaskPage.class);
+        startActivity(newActivity);
+    }
+    public void goToPeople(MenuItem menuItem){
+        Intent newActivity = new Intent(TaskPage.this, PeopleActivity.class);
+        startActivity(newActivity);
+    }
+
+    public void goToShopping(MenuItem menuItem){
+        Intent newActivity = new Intent(TaskPage.this, Shopping.class);
+        startActivity(newActivity);
+    }
+
+    public void goToLogin(MenuItem menuItem){
+        Intent newActivity = new Intent(TaskPage.this, UserLogin.class);
+        startActivity(newActivity);
+    }
+
+
+
+
+
 
 }
 
