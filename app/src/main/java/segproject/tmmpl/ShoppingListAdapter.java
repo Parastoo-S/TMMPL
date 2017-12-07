56af3a588104d0687be6e44c2d62c57901e852cf
Lori Antranikian <loriantranikian@yahoo.ca>
2017-12-06 19:48:47 -0500

    coments

--- app/src/main/java/segproject/tmmpl/ShoppingListAdapter.java
+++ app/src/main/java/segproject/tmmpl/ShoppingListAdapter.java
@@ -1,20 +1,13 @@
 package segproject.tmmpl;
 
 import android.app.Activity;
-import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
-
-/**
- * Created by Parastoo on 12/2/2017.
- */
 
 public class ShoppingListAdapter extends ArrayAdapter<Item> {
 
@@ -34,7 +27,6 @@ public class ShoppingListAdapter extends ArrayAdapter<Item> {
         View listViewItem = inflater.inflate(R.layout.layout_shopping_list_adapter, null, true);
 
         TextView itemName = (TextView) listViewItem.findViewById(R.id.itemName);
-//        TextView textViewPrice = (TextView) listViewItem.findViewById(R.id.textViewPrice);
 
         Item item = items.get(position);
         itemName.setText(item.getItemName());
