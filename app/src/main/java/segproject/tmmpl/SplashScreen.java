c9c9ead7a059460537342ad5e2b3581495394d60
Lori Antranikian <loriantranikian@yahoo.ca>
2017-12-06 19:49:03 -0500

    commetns

--- app/src/main/java/segproject/tmmpl/SplashScreen.java
+++ app/src/main/java/segproject/tmmpl/SplashScreen.java
@@ -7,21 +7,20 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
-    //private TextView textView;
-    //private ImageView imageView;
+    //this code was inspired from the following links
+    // https://android.jlelse.eu/right-way-to-create-splash-screen-on-android-e7f1709ba154
+    //www.youtube.com/watch?v=s8nuJpyQqVc
 
     protected void onPause(){
         super.onPause();
         //stops music rom playing
         ourSplashMusic.release();
-        //destroys activty and restarts when app opens
+        //destroys activity and restarts when app opens
         finish();
     }
 
     MediaPlayer ourSplashMusic;
     //global variable
-
-
 
     private static int SPLASH_TIME_OUT=4000;
 
@@ -29,19 +28,19 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
         ourSplashMusic = MediaPlayer.create(this,R.raw.angelicmusic);
         ourSplashMusic.start();
-        //object and refrence for sound
+        //object and reference for sound
 
 
-
+        //sets the layout view at the activity_splash_screen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
 
+                //sets where the next intent is to be after splash screen
                 Intent homeIntent= new Intent(SplashScreen.this, UserLogin.class);
 
                 startActivity(homeIntent);
@@ -49,7 +48,7 @@ public class SplashScreen extends AppCompatActivity {
             }
 
 
-        },SPLASH_TIME_OUT);
+        },SPLASH_TIME_OUT);//after 4 seconds, stops and moves to User Login screen
 
     }
 
